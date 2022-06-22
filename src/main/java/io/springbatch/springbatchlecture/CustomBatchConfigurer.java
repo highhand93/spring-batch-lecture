43b4bb84b8a4a0b17aec583d20015d3001e15470
highhand93 <programmerjong@gmail.com>
2022-06-22 09:21:55 +0900

    test: 커밋 테스트

--- src/main/java/io/springbatch/springbatchlecture/CustomBatchConfigurer.java
+++ src/main/java/io/springbatch/springbatchlecture/CustomBatchConfigurer.java
@@ -31,5 +31,6 @@ public class CustomBatchConfigurer extends BasicBatchConfigurer {
         jobRepositoryFactoryBean.setTablePrefix("SYSTEM_");
 
         return jobRepositoryFactoryBean.getObject();
+
     }
 }
